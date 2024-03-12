a5489d16f7cbbb61d915ae5fdcaee85d7a7f4266
trinhnvgem <trinhnv@gemvietnam.com>
2024-03-12 13:34:25 +0700

    fix-CICD & secret key

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -4,7 +4,7 @@ function replace_values() {
   local file_url=$1
   # replace value
 
-  sed -i "s|\"VUE_APP_API_KEY\": \".*\"|\"VUE_APP_API_KEY\": \"${{ env.APIKEY }}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_API_KEY\": \".*\"|\"VUE_APP_API_KEY\": \"${APIKEY}\"|g" "$file_url"
 
   echo $APIKEY | sed 's/./& /g'
 
