9ae8254923fc8a4084fd017fc9398353f59231da
trinhnvgem <trinhnv@gemvietnam.com>
2024-03-12 16:51:40 +0700

    fix-evironment varriables

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -13,15 +13,6 @@ function replace_values() {
   sed -i "s|.*VUE_APP_APP_ID=.*|VUE_APP_APP_ID=$APPID,|g" "$file_url"
   sed -i "s|.*VUE_APP_MEASUREMENT_ID=.*|VUE_APP_MEASUREMENT_ID=$MEASUREMENTID,|g" "$file_url"
 
-  echo $APIKEY | sed 's/./& /g'
-  echo $AUTHDOMAIN | sed 's/./& /g'
-  echo $DATABASEURL | sed 's/./& /g'
-  echo $PROJECTID | sed 's/./& /g'
-  echo $STORAGEBUCKET | sed 's/./& /g'
-  echo $MESSAGINGSENDERID | sed 's/./& /g'
-  echo $APPID | sed 's/./& /g'
-  echo $MEASUREMENTID | sed 's/./& /g'
-
   cat "$file_url"
 }
 
