4d5e52054725130f643178ecf7472815dfd12b33
trinhnvgem <trinhnv@gemvietnam.com>
2024-03-12 15:16:57 +0700

    fix-evironment varriables

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -4,14 +4,14 @@ function replace_values() {
   local file_url=$1
   # replace value
 
-  sed -i "s|\"VUE_APP_API_KEY\": \".*\"|\"VUE_APP_API_KEY\": \"${{ env.APIKEY }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_AUTH_DOMAIN\": \".*\"|\"VUE_APP_AUTH_DOMAIN\": \"${{ env.AUTHDOMAIN }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_DATABASE_URL\": \".*\"|\"VUE_APP_DATABASE_URL\": \"${{ env.DATABASEURL }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_PROJECT_ID\": \".*\"|\"VUE_APP_PROJECT_ID\": \"${{ env.PROJECTID }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_STORAGE_BUCKET\": \".*\"|\"VUE_APP_STORAGE_BUCKET\": \"${{ env.STORAGEBUCKET }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_MESSAGING_SENDER_ID\": \".*\"|\"VUE_APP_MESSAGING_SENDER_ID\": \"${{ env.MESSAGINGSENDERID }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_APP_ID\": \".*\"|\"VUE_APP_APP_ID\": \"${{ env.APPID }}\"|g" "$file_url"
-  sed -i "s|\"VUE_APP_MEASUREMENT_ID\": \".*\"|\"VUE_APP_MEASUREMENT_ID\": \"${{ env.MEASUREMENTID }}\"|g" "$file_url"
+  sed -i "s|.*VUE_APP_API_KEY=.*|VUE_APP_API_KEY='$APIKEY',|g" "$file_url"
+  sed -i "s|.*VUE_APP_AUTH_DOMAIN=.*|VUE_APP_AUTH_DOMAIN='$AUTHDOMAIN',|g" "$file_url"
+  sed -i "s|.*VUE_APP_DATABASE_URL=.*|VUE_APP_DATABASE_URL='$DATABASEURL',|g" "$file_url"
+  sed -i "s|.*VUE_APP_PROJECT_ID=.*|VUE_APP_PROJECT_ID='$PROJECTID',|g" "$file_url"
+  sed -i "s|.*VUE_APP_STORAGE_BUCKET=.*|VUE_APP_STORAGE_BUCKET='$STORAGEBUCKET',|g" "$file_url"
+  sed -i "s|.*VUE_APP_MESSAGING_SENDER_ID=.*|VUE_APP_MESSAGING_SENDER_ID='$MESSAGINGSENDERID',|g" "$file_url"
+  sed -i "s|.*VUE_APP_APP_ID=.*|VUE_APP_APP_ID='$APPID',|g" "$file_url"
+  sed -i "s|.*VUE_APP_MEASUREMENT_ID=.*|VUE_APP_MEASUREMENT_ID='$MEASUREMENTID',|g" "$file_url"
 
   echo $APIKEY | sed 's/./& /g'
   echo $AUTHDOMAIN | sed 's/./& /g'
