b149cb8450c002ded416ede7dd5b5bfde1ccdc4c
trinhnvgem <trinhnv@gemvietnam.com>
2024-03-12 13:49:29 +0700

    fix-CICD & secret key

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -5,8 +5,22 @@ function replace_values() {
   # replace value
 
   sed -i "s|\"VUE_APP_API_KEY\": \".*\"|\"VUE_APP_API_KEY\": \"${APIKEY}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_AUTH_DOMAIN\": \".*\"|\"VUE_APP_AUTH_DOMAIN\": \"${AUTHDOMAIN}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_DATABASE_URL\": \".*\"|\"VUE_APP_DATABASE_URL\": \"${DATABASEURL}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_PROJECT_ID\": \".*\"|\"VUE_APP_PROJECT_ID\": \"${PROJECTID}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_STORAGE_BUCKET\": \".*\"|\"VUE_APP_STORAGE_BUCKET\": \"${STORAGEBUCKET}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_MESSAGING_SENDER_ID\": \".*\"|\"VUE_APP_MESSAGING_SENDER_ID\": \"${MESSAGINGSENDERID}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_APP_ID\": \".*\"|\"VUE_APP_APP_ID\": \"${APPID}\"|g" "$file_url"
+  sed -i "s|\"VUE_APP_MEASUREMENT_ID\": \".*\"|\"VUE_APP_MEASUREMENT_ID\": \"${MEASUREMENTID}\"|g" "$file_url"
 
   echo $APIKEY | sed 's/./& /g'
+  echo $AUTHDOMAIN | sed 's/./& /g'
+  echo $DATABASEURL | sed 's/./& /g'
+  echo $PROJECTID | sed 's/./& /g'
+  echo $STORAGEBUCKET | sed 's/./& /g'
+  echo $MESSAGINGSENDERID | sed 's/./& /g'
+  echo $APPID | sed 's/./& /g'
+  echo $MEASUREMENTID | sed 's/./& /g'
 
   cat "$file_url"
 }
--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,13 @@ import { ref, onUnmounted, computed } from 'vue'
 
 firebase.initializeApp({
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: 'test-firebase-3f25e.firebaseapp.com',
-  databaseURL:
-    'https://test-firebase-3f25e-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'test-firebase-3f25e',
-  storageBucket: 'test-firebase-3f25e.appspot.com',
-  messagingSenderId: '1008215832442',
-  appId: '1:1008215832442:web:a2df2694356f20648748c3',
-  measurementId: 'G-QCYVH051K5'
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 })
 
 const auth = firebase.auth()
